07748298e0dedcfa60ee324b67b007445ee0b2a7
MAJESTIC123 <karthiece996@gmail.com>
2017-08-04 14:42:22 +0530

    Add files via upload

---
+++ Hunter95.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Hunt 95 
+{
+
+	public static void main(String[] args) 
+{
+	Scanner s=new Scanner(System.in);
+	int n=s.nextInt();
+
+	for(int i=0;i<100;i++)
+{
+		if((i%2!=0&&i%3!=0)||i==2||i==3)
+{
+			
+		
+}
+	
+}
+	
+}
+
+}
